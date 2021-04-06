5aa00c2c865e2ae641b33b115f657c7ab324d825
Chris Burton <chris@fullstackcaveman.com>
2021-04-06 12:11:44 -0600

    Got server running

--- api/server.js
+++ api/server.js
@@ -1,2 +1,14 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+	res.send('Server Running');
+});
+
+module.exports = server;
